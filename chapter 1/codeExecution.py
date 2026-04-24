34aa276adb1a1145f42aab39a6fd13a79925cbab
Priyanka Chaurasiya <priyankachaurasiya6509@gmail.com>
2026-04-24 13:16:02 +0530

    Add files via upload

---
+++ chapter 1/codeExecution.py
@@ -0,0 +1,7 @@
+name = "priyanka chaurasiya"
+age1 = 19
+age2 = 20
+print("Actual  value:" ,age2)
+favSubject = "Maths"
+age2 = age1
+print("Changed value:" ,age2)
---
+++ chapter 1/userinput.py
@@ -0,0 +1,8 @@
+# Program to take input from the user
+
+print("A sample program to understand input methode in python")
+
+name= input("Enter your goood name :")
+age= input("Enter your age :")
+print("your good name is :",name)
+print("your age is :",age)
